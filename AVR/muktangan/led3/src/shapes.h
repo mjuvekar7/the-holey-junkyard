2a61183c19448fabc78061c79e4e61988bd29c69
mjuvekar7 <mjuvekar7@83a50805-d1b6-c788-a754-61f2260299cb>
2014-02-26 08:51:20 +0000

    Added shift functions for line,rectangle and point in transform.h. Fixed some minor bugs. Minor edits done

--- AVR/muktangan/led3/src/shapes.h
+++ AVR/muktangan/led3/src/shapes.h
@@ -11,7 +11,7 @@ struct Point
 
 struct Line
 {
-    struct Point p0, p1;
+    struct Point s, e;
 }
 
 struct Rect
@@ -38,9 +38,9 @@ void toggle_l (struct Line *l)
     float t;
     for (t = 0; t <= 1; t += 0.05)
     {
-        x = (l->p0.x + l->p1.x * t + 0.5);
-        y = (l->p0.y + l->p1.y * t + 0.5);
-        z = (l->p0.z + l->p1.z * t + 0.5);
+        x = (l->s.x + l->e.x * t + 0.5);
+        y = (l->s.y + l->e.y * t + 0.5);
+        z = (l->s.z + l->e.z * t + 0.5);
         full_buf[x][y] ^= (1 << z);
     }
 }
@@ -49,27 +49,27 @@ void toggle_r (struct Rect *r)
 {
     struct Line s0, s1, s3, s4;
 
-    s0.p0 = r->c;
-    s0.p1 = r->p0;
+    s0.s = r->c;
+    s0.e = r->p0;
 
-    s1.p0 = r->c;
-    s1.p1 = r->p1;
+    s1.s = r->c;
+    s1.e = r->p1;
 
     struct Point d;
     d.x = r->p0.x - r->c.x + r->p1.x;
     d.y = r->p0.y - r->c.y + r->p1.y;
     d.z = r->p0.z - r->c.z + r->p1.z;
     
-    s2.p0 = r->p0;
-    s2.p1 = d;
+    s2.s = r->p0;
+    s2.e = d;
 
-    s3.p0 = r->p1;
-    s3.p1 = d;
+    s3.s = r->p1;
+    s3.e = d;
 
-    toggle_l(s0);
-    toggle_l(s1);
-    toggle_l(s2);
-    toggle_l(s3);
+    toggle_l(&s0);
+    toggle_l(&s1);
+    toggle_l(&s2);
+    toggle_l(&s3);
 }
 
 #endif
--- AVR/muktangan/led3/src/transform.h
+++ AVR/muktangan/led3/src/transform.h
@@ -1,3 +1,6 @@
+#ifndef _TRANSFORM_H
+#define _TRANSFORM_H
+
 #include <avr/io.h>
 #include "shapes.h"
 
@@ -51,3 +54,69 @@ void shift (int8_t dir, uint8_t mag)
                     full_buf[i][j] = full_buf[i][j] >> mag;
 }
 
+void shift_p (struct Point *p, int8_t dir, uint8_t mag)
+{
+    if (dir < 0)
+    {
+	if (dir > -3)
+	    if (dir > -2)
+		p.x = p.x - mag;
+	    else
+		p.y = p.y - mag;
+	else
+	    if (dir > -4)
+		p.z = p.z - mag;
+	    else
+#               warning "Arguement value outside required range"
+    }
+    else if (dir > 0)
+    {
+	if (dir < 3)
+	    if (dir < 2)
+		p.x = p.x + mag;
+	    else
+		p.y = p.y + mag;
+	else
+	    if (dir < 4)
+		p.z  = p.z + mag;
+	    else
+#               warning "Arguement value outside required range"
+    }
+    else
+#               warning "Arguement value outside required range"
+}
+
+void shift_l (struct Line *l, int8_t dir, uint8_t mag)
+{
+    shift_p (&l.s, dir, mag);
+    shift_p (&l.e, dir, mag);
+}
+
+void shift_r (struct Rect *r, int8_t dir, uint8_t mag)
+{
+    struct Line s0, s1, s2, s3;
+   
+    s0.s = r->c;
+    s0.e = r->p0;
+
+    s1.s = r->c;
+    s1.e = r->p1;
+
+    struct Point d;
+    d.x = r->p0.x - r->c.x + r->p1.x;
+    d.y = r->p0.y - r->c.y + r->p1.y;
+    d.z = r->p0.z - r->c.z + r->p1.z;
+    
+    s2.s = r->p0;
+    s2.e = d;
+
+    s3.s = r->p1;
+    s3.e = d;
+
+    shift_l(&s0, dir, mag);
+    shift_l(&s1, dir, mag);
+    shift_l(&s2, dir, mag);
+    shift_l(&s3, dir, mag);
+}
+
+#endif
