0d9df09e8bf152f83a1e97bf379129f575b19e25
shardul.chiplunkar <shardul.chiplunkar@83a50805-d1b6-c788-a754-61f2260299cb>
2014-07-05 08:24:14 +0000

    basic mathinator stuff

---
+++ trunk/mathinator/parser.py
@@ -0,0 +1,17 @@
+import ply.lex as lex
+import ply.yacc as yacc
+
+tokens = [
+    'VERB',
+    'PUNC',
+    'CONJ',
+    'DETR',
+    'ID',
+    'WHAT',
+    'HOWMANY',
+    'BE',
+    'PROP',
+    'ADJECTIVE'
+]
+
+
