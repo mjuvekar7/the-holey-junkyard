80549d0f785b438adb2ebcdb89340f583cfa15f2
mjuvekar7 <mjuvekar7@83a50805-d1b6-c788-a754-61f2260299cb>
2014-08-11 12:04:24 +0000

    Added alternate blink function to blinker. Deleted alternate blinker file.

--- trunk/GPIO/blinker/blinker.py
+++ trunk/GPIO/blinker/blinker.py
@@ -2,11 +2,26 @@ import RPi.GPIO as GPIO
 import time
 
 def blink(out, delay):
+    GPIO.cleanup()
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(out, GPIO.OUT)
     while True:
         GPIO.output(out, 1)
         time.sleep(delay)
         GPIO.output(out, 0)
+        time.sleep(delay)
+
+def blink_alt(out0, out1, delay):
+    GPIO.cleanup()
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(out0, GPIO.OUT)
+    GPIO.setup(out1, GPIO.OUT)
+
+    while True:
+        GPIO.output(out0, 1)
+        GPIO.output(out1, 0)
+        time.sleep(delay)
+        GPIO.output(out0, 0)
+        GPIO.output(out1, 1)
         time.sleep(delay)
 
