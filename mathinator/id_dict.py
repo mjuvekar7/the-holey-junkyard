af51cd2f2b81c271d7a50a62c909aca4b3eea7b7
shardul.chiplunkar <shardul.chiplunkar@gmail.com>
2014-09-03 13:33:33 +0000

    Added ignore rule for __pycache__ and parser files

--- mathinator/id_dict.py
+++
@@ -1,9 +0,0 @@
-# of the form id (string) : Attribute
-ids = {}
-
-# Attribute can contain *anything*, from a single number value to arrays of
-# items possessed by IDs to height/weight of IDs, etc.
-class Attribute:
-    def __init__(self, name='', value=0):
-        self.name = name
-        self.value = value
