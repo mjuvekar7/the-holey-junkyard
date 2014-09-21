f44f4e5150a5723c198f2a6e2dc7d4e67215a56d
mjuvekar7 <mjuvekar7@83a50805-d1b6-c788-a754-61f2260299cb>
2014-09-21 16:15:08 +0000

    temporary commit (please omit)

--- trunk/GPIO/blinker/blinker.py
+++ trunk/GPIO/blinker/blinker.py
@@ -1,27 +1,25 @@
-import RPi.GPIO as GPIO
+import RPi.GPIO as gpio
 import time
 
 def blink(out, delay):
-    GPIO.cleanup()
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(out, GPIO.OUT)
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(out, gpio.OUT)
     while True:
-        GPIO.output(out, 1)
+        gpio.output(out, 1)
         time.sleep(delay)
-        GPIO.output(out, 0)
+        gpio.output(out, 0)
         time.sleep(delay)
 
 def blink_alt(out0, out1, delay):
-    GPIO.cleanup()
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(out0, GPIO.OUT)
-    GPIO.setup(out1, GPIO.OUT)
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(out0, gpio.OUT)
+    gpio.setup(out1, gpio.OUT)
 
     while True:
-        GPIO.output(out0, 1)
-        GPIO.output(out1, 0)
+        gpio.output(out0, 1)
+        gpio.output(out1, 0)
         time.sleep(delay)
-        GPIO.output(out0, 0)
-        GPIO.output(out1, 1)
+        gpio.output(out0, 0)
+        gpio.output(out1, 1)
         time.sleep(delay)
 
